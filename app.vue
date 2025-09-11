8a1ecc640dd69d1b1dcfc2168d910f3b36e997b8
Vadizar <vadizar@gmail.com>
2025-09-11 15:44:07 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -17,7 +17,6 @@ useHead({
 
 <template>
     <Menu />
-
     <main class="flex-1 min-h-screen p-0 bg-white/95 backdrop-blur-md text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors md:ml-72 lg:ml-[300px]">
         <NuxtPage />
     </main>
